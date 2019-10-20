7272abe7985e3a7dc750c5b58f8ca172e31a69f9
Ciro S. Costa <cscosta@pivotal.io>
2019-10-20 18:41:42 -0400

    infra: add cg to installation
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- infra/install.sh
+++ infra/install.sh
@@ -6,8 +6,16 @@ set -o nounset
 readonly NE_VERSION=0.18.1
 readonly NE_URL="https://github.com/prometheus/node_exporter/releases/download/v$NE_VERSION/node_exporter-$NE_VERSION.linux-amd64.tar.gz"
 
+readonly CG_URL="https://github.com/cirocosta/chicken-gun/releases/download/0.0.1-rc1/cg"
+
 main () {
 	install_node_exporter
+	install_cg
+}
+
+install_cg () {
+	curl -SOL $CG_URL
+	sudo install -m 0755 ./cg /usr/local/bin
 }
 
 install_node_exporter () {
